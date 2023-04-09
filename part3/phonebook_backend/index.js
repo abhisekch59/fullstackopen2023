d8972e78745a2314581a3bf017f02f11d5289271
abhisekch59 <abhisekch59@gmail.com>
2023-04-09 18:03:44 +0530

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
