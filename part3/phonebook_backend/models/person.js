d412eea050b59a2018163b3a31c5f55fee7dcc27
abhisekch59 <abhisekch59@gmail.com>
2023-04-21 22:24:46 +0530

    uibuild

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -7,52 +7,52 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: function(v) {
-                const splittedNumber = v.split('-')
-                if(splittedNumber.length !== 2) {
-                    return false
-                }
-                const firstPart = splittedNumber[0]
-                const secondPart = splittedNumber[1]
-                let isnum1 = /^\d+$/.test(firstPart)
-                let isnum2 = /^\d+$/.test(secondPart)
-                if(!isnum1 || !isnum2) {
-                    return false
-                }
-                if(firstPart.length <= 1 || firstPart.length > 3) {
-                    return false
-                }
-                return true
-            },
-            message: props => `${props.value} is not a valid phone number!`
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        const splittedNumber = v.split('-')
+        if(splittedNumber.length !== 2) {
+          return false
         }
+        const firstPart = splittedNumber[0]
+        const secondPart = splittedNumber[1]
+        let isnum1 = /^\d+$/.test(firstPart)
+        let isnum2 = /^\d+$/.test(secondPart)
+        if(!isnum1 || !isnum2) {
+          return false
+        }
+        if(firstPart.length <= 1 || firstPart.length > 3) {
+          return false
+        }
+        return true
+      },
+      message: props => `${props.value} is not a valid phone number!`
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
